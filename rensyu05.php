ff3da0a66d92848e65dc424918731b4af9fdeced
sakura2317 <blackbutterfly.0929@gmail.com>
2015-06-25 14:25:19 +0900

    追加

---
+++ rensyu05.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>練習</title>
+</head>
+
+<body>
+<?php
+
+define("TSUBO",3.3);
+printf("101号室→%.2f坪<br>",100/TSUBO);
+printf("102号室→%.2f坪<br>", 75/TSUBO);
+printf("103号室→%.2f坪    ",150/TSUBO);
+
+?>
+
+</body>
+</html>
---
+++ rensyu06.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+<?php
+$color1 = "#E1E1BC";
+$color2 = "#E7E7E7";
+$color3 = "red";
+
+
+?>
+
+<table border="1">
+  <tr style>
+    <th>県名</th>
+    <th>総人口（千人）</th>
+    <th>男性（千人）</th>
+    <th>女性（千人）</th>
+    <th></th>
+  </tr>
+  <tr>
+    <td>青森県</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>岩手県</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>秋田県</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+
+
+
+</body>
+</html>
